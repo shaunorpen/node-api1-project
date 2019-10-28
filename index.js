ec490ff5f0f7c53ae6ece55ee79e39f44c05e993
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-28 12:22:54 +0000

    Server working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get('*', handleDefaultRequest);
+
+function handleDefaultRequest(request, response) {
+    response.json('Hello World!');
+}
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on ' + (process.env.PORT || 3000));
+});
