36a92dcfcf03c833d6fcde04c613e4c43be11604
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-28 13:49:30 +0000

    Add edit user functionality

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createNewUser(request, response) {
     })
     .catch(err => {
         return response.status(500).json({ error: "There was an error while saving the user to the database" });
-    })
+    });
 }
 
 server.get('/api/users', getAllUsers);
@@ -41,6 +41,7 @@ server.get('/api/users/:id', getUserById);
 
 function getUserById(request, response) {
     const { id } = request.params;
+
     db.findById(id)
     .then(user => {
         if (!user) {
@@ -50,13 +51,14 @@ function getUserById(request, response) {
     })
     .catch(error => {
         response.status(500).json({ error: "The user information could not be retrieved." });
-    })
+    });
 }
 
 server.delete('/api/users/:id', deleteUserById);
 
 function deleteUserById(request, response) {
     const { id } = request.params;
+
     db.remove(id)
     .then(res => {
         if (!res) {
@@ -66,36 +68,29 @@ function deleteUserById(request, response) {
     })
     .catch(err => {
         return response.status(500).json(err);
-    })
+    });
 }
 
 server.put('/api/users/:id', editUserById);
 
 function editUserById(request, response) {
-    // When the client makes a `PUT` request to `/api/users/:id`:
+    const { id } = request.params;
+    const user = request.body;
+
+    if (!user.name || !user.bio) {
+        return response.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
     
-    // - If the _user_ with the specified `id` is not found:
-    
-    //   - return HTTP status code `404` (Not Found).
-    //   - return the following JSON object: `{ message: "The user with the specified ID does not exist." }`.
-    
-    // - If the request body is missing the `name` or `bio` property:
-    
-    //   - cancel the request.
-    //   - respond with HTTP status code `400` (Bad Request).
-    //   - return the following JSON response: `{ errorMessage: "Please provide name and bio for the user." }`.
-    
-    // - If there's an error when updating the _user_:
-    
-    //   - cancel the request.
-    //   - respond with HTTP status code `500`.
-    //   - return the following JSON object: `{ error: "The user information could not be modified." }`.
-    
-    // - If the user is found and the new information is valid:
-    
-    //   - update the user document in the database using the new information sent in the `request body`.
-    //   - return HTTP status code `200` (OK).
-    //   - return the newly updated _user document_.
+    db.update(id, user)
+    .then(userId => {
+        if (!userId) {
+            return response.status(404).json({ message: "The user with the specified ID does not exist." });
+        }
+        return response.status(200).json(userId);
+    })
+    .catch(err => {
+        response.status(500).json({ error: "The user information could not be modified." });
+    });
 }
 
 server.get('*', handleDefaultRequest);
