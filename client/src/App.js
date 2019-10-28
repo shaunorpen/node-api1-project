b2a5b370004eb578904fb90dec82a97cedf8b744
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-28 14:31:05 +0000

    Add client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [updatedUser, setUpdatedUser] = useState(0);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/users')
+    .then(res => {
+      setUsers(res.data);
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+  }, [updatedUser]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users.map(user => <UserCard user={user} key = {user.id} setUpdatedUser={setUpdatedUser} />)
+      }
+    </div>
+  )
+}
+
+function deleteUser(id, setUpdatedUser) {
+  axios.delete(`http://localhost:3000/api/users/${id}`)
+  .then(res => {
+    debugger
+    setUpdatedUser(res);
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+}
+
+function UserCard({ user, setUpdatedUser }) {
+  return (
+    <div>
+      <p>User Name: {user.name}</p>
+      <p>User Bio: {user.bio}</p>
+      <button onClick={() => deleteUser(user.id, setUpdatedUser)} >Delete</button>
     </div>
   );
 }
